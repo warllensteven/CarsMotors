12f7f547d97e27debe7dfee57562303781367716
warllensteven <warlenromero14@gmail.com>
2025-04-29 20:49:22 -0500

    Initial project structure with Maven and MVC folders

---
+++ src/main/java/com/carmotors/CarMotorsWorkshop.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.carmotors;
+
+/**
+ *
+ * @author warle
+ */
+public class CarMotorsWorkshop {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
